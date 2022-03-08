ed228a84ddb4c44d3f2f2ba6e2aedd1750dd3e2f
victorfunch <victor_funch@hotmail.com>
2022-03-08 16:08:39 +0100

    added to mymodule

--- class5/mymodule.py
+++ class5/mymodule.py
@@ -1,3 +1,8 @@
 def myfun(n):
     for i in range(n):
         print('hello world!')
+        
+def quick_math(number1, number2):
+    _sum = number1 + number2
+    _product = number1 * number2
+    return _sum, _product
